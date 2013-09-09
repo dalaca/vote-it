a0e60fc78c1c52fa0da6ef73f3d654aa00556a95
Dave C <david1cardoso@gmail.com>
2013-09-08 21:51:44 -0400

    Controller Creations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,39 @@
 class PostsController < ApplicationController
   def index
-
+  	@post = Post.all
   end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+ 		
+ 		if @post.save
+ 			redirect_to @post
+ 		else
+ 			render 'new'
+ 		end
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  	render 'show'
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update(params[:post])
+  		redirect_to @post
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
 end
