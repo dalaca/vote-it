f98bb39dfda51cc2e120562dcf40790a69290da4
Dave C <david1cardoso@gmail.com>
2013-09-09 23:48:04 -0400

    added form on new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
   end
 
   def create
-  	@post = Post.new(post_params)
- 		
+  	@post = Post.new
+    
  		if @post.save
  			redirect_to @post
  		else
